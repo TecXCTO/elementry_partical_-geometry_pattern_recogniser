3f0f8a49785043f021de90924369ac384bc13d5f
TecXCTO <cto.tecx@gmail.com>
2026-08-27 11:19:58 +0530

    Fix variable assignments and calculate PRRF

--- src/prrf/primitive_radius_at_respective_frequency.py
+++ src/prrf/primitive_radius_at_respective_frequency.py
@@ -1,7 +1,9 @@
 # PRRF: Primitive Radius at Respective Frequency
 
 
-partical_frequency = 0
-partical_phase = = 0
-phase_difference= 0, 120, 180, 240
-partical_spin = 0.333, .5, .666,
+pf = 0 # Partical Frequency
+pp = = 0 # partical_phase Partical Phase
+pd = 0, 120, 180, 240 # phase_difference Phase Difference
+ps = 0.333, .5, .666 # partical_spin Partical Spin
+#primitive_radaius
+prrf = pf*ps # Primitive Radius at Respective Frequency
