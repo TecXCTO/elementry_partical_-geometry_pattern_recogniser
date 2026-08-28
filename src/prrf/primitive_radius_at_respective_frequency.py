07cf9f8f6b9637cacf6ee35c7e82e0c712ab4706
TecXCTO <cto.tecx@gmail.com>
2026-08-28 17:47:13 +0530

    Update primitive_radius_at_respective_frequency.py

--- src/prrf/primitive_radius_at_respective_frequency.py
+++ src/prrf/primitive_radius_at_respective_frequency.py
@@ -1,5 +1,16 @@
 # PRRF: Primitive Radius at Respective Frequency
 
+"""
+ParticleTypeSpin Quantum Number (\(s\))Rest Mass Frequency (\(f_{0}\) in Hz)
+
+Up Quark (\(u\))Fundamental (Fermion)1/2\(\approx 5.3 \times 10^{21}\text{ Hz}\)
+Down Quark (\(d\))Fundamental (Fermion)1/2\(\approx 1.1 \times 10^{22}\text{ Hz}\)
+Electron (\(e^{-}\))Fundamental (Fermion)1/2\(\approx 1.23 \times 10^{20}\text{ Hz}\)
+Proton (\(p\))Composite (Baryon: \(uud\))1/2\(\approx 2.27 \times 10^{23}\text{ Hz}\)
+Neutron (\(n\))Composite (Baryon: \(udd\))1/2\(\approx 2.27 \times 10^{23}\text{ Hz}\)
+Hydrogen (\(^1\text{H}\))Composite Atom (\(p + e^-\))0 (Antiparallel) or 1 (Parallel)
+"""
+
 class ElementaryParticle:
   def __init__(partical_spin = 0.5, partical_frequency = None):
   permittivity =
@@ -7,5 +18,6 @@ class ElementaryParticle:
   pf = partical_frequency # 0 # Partical Frequency
   pp = = 0 # partical_phase Partical Phase
   pd = 0, 120, 180, 240 # phase_difference Phase Difference
-  ps = partical_spin # 0.333, .5, .666 # partical_spin Partical Spin 
+  ps = partical_spin # 0.5 # partical_spin Partical Spin 
+  pc =  # Partical Charge # -0.333, 0.5, 0.666 
   prrf = ps/(2*math.pi*pf*math.sqrt(prmtvt*permblt)) # Primitive Radius at Respective Frequency # primitive_radaius
