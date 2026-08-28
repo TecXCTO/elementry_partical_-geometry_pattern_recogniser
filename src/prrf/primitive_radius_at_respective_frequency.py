2d8471c840570e303576d8e9aaf842c8b161229e
TecXCTO <cto.tecx@gmail.com>
2026-08-28 10:18:31 +0530

    Update primitive_radius_at_respective_frequency.py

--- src/prrf/primitive_radius_at_respective_frequency.py
+++ src/prrf/primitive_radius_at_respective_frequency.py
@@ -1,10 +1,11 @@
 # PRRF: Primitive Radius at Respective Frequency
 
-permittivity =
-permeability =
-pf = 0 # Partical Frequency
-pp = = 0 # partical_phase Partical Phase
-pd = 0, 120, 180, 240 # phase_difference Phase Difference
-ps = 0.333, .5, .666 # partical_spin Partical Spin
-
-prrf = (1/pf)*ps*math.sqrt(prmtvt*permblt) # Primitive Radius at Respective Frequency # primitive_radaius
+class ElementaryParticle:
+  def __init__(partical_spin = 0.5, partical_frequency = None):
+  permittivity =
+  permeability =
+  pf = partical_frequency # 0 # Partical Frequency
+  pp = = 0 # partical_phase Partical Phase
+  pd = 0, 120, 180, 240 # phase_difference Phase Difference
+  ps = partical_spin # 0.333, .5, .666 # partical_spin Partical Spin 
+  prrf = ps/(2*math.pi*pf*math.sqrt(prmtvt*permblt)) # Primitive Radius at Respective Frequency # primitive_radaius
